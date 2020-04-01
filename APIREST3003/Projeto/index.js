b99e1c725eee78675eae29edc4798df0b7230d9e
thiagoagduarte <thiagoagduarte@gmail.com>
2020-04-01 18:01:42 -0300

    Aula 3003

---
+++ APIREST3003/Projeto/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let rawbooks = fs.readFileSync('books.json')
+let books = JSON.parse(rawbooks);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/book', (req, res) => {
+    res.json(books);
+});
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+    if(Array.isArray(book)){
+        for (item of book){
+            books.push(item);
+        }
+    }
+    else{
+        books.push(book);
+    }
+    let jsonList = JSON.stringify(books);
+    fs.writeFile('books.json', jsonList, 'utf8', () => { });
+    res.send('Livro cadastrado com sucesso!');
+});
+
+app.get('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    for (let book of books){
+        if(book.ISBN === isbn){
+            res.json(book);
+            return;
+        }
+    }
+    res.status(404).send('Livro não encontrado');
+});
+
+app.listen(3000);
